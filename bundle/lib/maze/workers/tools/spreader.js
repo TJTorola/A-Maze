5bd322c77ef5893643520886c7bfdc697be62e7b
Tyler Torola <tyler@tjt.codes>
2016-09-21 17:04:52 -0700

    builds basic spreader tool

---
+++ bundle/lib/maze/workers/tools/spreader.js
@@ -0,0 +1,56 @@
+import Tool from '../tool';
+import { DIR_DATA } from 'utilities/constants';
+
+export default class extends Tool {
+	constructor(controller, start) {
+		super(controller);
+
+		this.stack = [start];
+		this.removeStatus('unvisited');
+	}
+
+	unblockedDirs(pos) {
+		const cell = this.grid[pos[0]][pos[1]];
+		const unblocked = [];
+
+		[ 'up', 'right', 'down', 'left' ].forEach(dir => {
+			if (!cell.walls.includes(dir)) { unblocked.push(dir); }
+		});
+
+		return unblocked;
+	}
+
+	possibleDirs(pos, className) {
+		const { pos } = this;
+		const { grid } = this.grid;
+		let cell, delta;
+		let possible = [];
+		
+		this.unblockedDirs(pos).forEach(dir => {
+			delta = DIR_DATA[dir].delta;
+			cell = grid[pos[0] + delta[0]][pos[1] + delta[1]];
+
+			if (cell.status.indexOf(className) != -1) {
+				possible.push(dir);
+			}
+		});
+
+		return possible;
+	}
+
+	ooze(className = 'spread', val = 0) {
+		let delta, newPos;
+		const sources = this.stack;
+		this.stack = [];
+
+		sources.forEach(pos => {
+			this.possibleDirs(pos, className).forEach(dir => {
+				delta = DIR_DATA[dir].delta;
+				newPos = [pos[0] + delta[0]][pos[1] + delta[1]];
+
+				this.addStatus('spread', newPos);
+				this.stack.push(newPos);
+			});
+		});
+	}
+}
