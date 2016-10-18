f677d67b9ab0d0008b0538bdafd1c42b64aae5ee
Tyler Torola <tyler@tjt.codes>
2016-10-18 10:39:42 -0700

    cleans new graph

--- bundle/flib/graph/new.js
+++ bundle/flib/graph/new.js
@@ -1,5 +1,5 @@
 import { DEFAULT_CELL } from 'utilities/constants'
 
-const cell = () => (DEFAULT_CELL);
-const row = x => () => new Array(x).fill(null).map(cell)
-export default (x, y) => new Array(y).fill(null).map(row(x))
+const cell = () => DEFAULT_CELL;
+const row = x => () => new Array(x).fill(null).map(cell);
+export default (x, y) => new Array(y).fill(null).map(row(x));
